11e62ec4f5dfe326b521de0762f8a3ebff7a99aa
Grigoriy Reuk <grishu@Grigoriys-MacBook-Air.local>
2024-04-24 19:06:59 +0300

    added html

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
+	app.Get("/hello", func(c fiber.Ctx) error {
 		// Send a string response to the client
 		return c.SendString("Hello, World 👋!")
 	})
@@ -17,6 +17,8 @@ func main() {
 	app.Get("/test", func(ctx fiber.Ctx) error {
 		return ctx.SendString("I'm alive")
 	})
+
+	app.Static("/", "./website")
 
 	log.Fatal(app.Listen(":8080"))
 }
